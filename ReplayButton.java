17d6cb9c210f61d3be976753571af61c5e3c58a3
Jocelyn Mathew <49656538+josiemath@users.noreply.github.com>
2025-12-02 00:21:39 -0800

    Add files via upload

---
+++ ReplayButton.java
@@ -0,0 +1,39 @@
+import greenfoot.*;
+
+public class ReplayButton extends Actor
+{
+    private static final GreenfootImage NORMAL_IMAGE = new GreenfootImage("replayButton.png");
+    private static final GreenfootImage HOVER_IMAGE = new GreenfootImage("replayButton.png");
+    
+    public ReplayButton()
+    {
+        // Set the initial image
+        setImage(NORMAL_IMAGE);
+    }
+
+    public void act()
+    {
+        // 1. Handle the click action (same as before)
+        if (Greenfoot.mouseClicked(this))
+        {
+            Greenfoot.setWorld(new PandaWorld());
+            return; 
+        }
+
+        // 2. Handle the hover action: Switch images
+        if (Greenfoot.mouseMoved(this))
+        {
+            // If the mouse is over the button, set the pre-faded image
+            if (getImage() != HOVER_IMAGE) {
+                setImage(HOVER_IMAGE);
+            }
+        }
+        else
+        {
+            // If the mouse is NOT over the button, set the normal image
+            if (getImage() != NORMAL_IMAGE) {
+                setImage(NORMAL_IMAGE);
+            }
+        }
+    }    
+}
